2c156e6bcb54842f974bd5e193dd189d5603b00a
ramananda1110 <cserama.tpi@gmail.com>
2025-02-27 16:01:46 +0600

    upgrade controller

--- app/Http/Controllers/DataScrapingController.php
+++ app/Http/Controllers/DataScrapingController.php
@@ -13,13 +13,13 @@ class DataScrapingController extends Controller
 {
     public function data_scraping()
     {
-        for ($i = 401000001; $i < 401000100; $i++) { 
+        for ($i = 401000062; $i < 401000200; $i++) { 
             $roll = $i;
 
             // Send POST request
             $response = Http::timeout(60)->asForm()->post('http://ntrca.teletalk.com.bd/result/index.php', [
                 'rollno'  => $roll,
-                'exam'    => '18:18th:2023:1',
+                'exam'    => '18:18th:2023:2',
                 'yes'     => 'YES',
                 'button2' => 'Submit',
             ]);
